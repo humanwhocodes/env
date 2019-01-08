9813fe269c4c731480c2f5811b8e0b4c12cdac92
Nicholas C. Zakas <nicholas@nczconsulting.com>
2019-01-08 09:29:55 -0800

    New: Implemented Env class

---
+++ src/env.js
@@ -0,0 +1,51 @@
+/**
+ * @fileoverview A utility for ensuring that environment variables are present.
+ */
+
+"use strict";
+
+/**
+ * A utility for interacting with environment variables
+ */
+class Env {
+
+    /**
+     * Creates a new instance of Env.
+     * @param {Object} source The environment variable object to read from. 
+     */
+    constructor(source = process.env) {
+        this.source = source;
+    }
+
+    /**
+     * Retrieves an environment variable without checking for its presence.
+     * Optionally returns a default value if the environment variable doesn't
+     * exist.
+     * @param {string} key The environment variable name to retrieve.
+     * @param {string} [defaultValue] The default value to return if the
+     *      environment variable is not found.
+     * @returns {string?} The environment variable value if found or null if not.
+     */
+    get(key, defaultValue = null) {
+        return (key in this.source) ? this.source[key] : defaultValue;
+    }
+
+    /**
+     * Retrieves an environment variable. If the environment variable does
+     * not exist, then it throws an error.
+     * @param {string} key The environment variable name to retrieve.
+     * @returns {string?} The environment variable value.
+     * @throws {Error} When the environment variable doesn't exist.
+     */
+    require(key) {
+        const value = this.get(key);
+        if (value === null) {
+            throw new Error(`Required environment variable '${key}' not found.`);
+        } else {
+            return value;
+        }
+    }
+
+}
+
+exports.Env = Env;
---
+++ tests/env.js
@@ -0,0 +1,85 @@
+/**
+ * @fileoverview Tests for the Env class.
+ */
+/*global describe, it*/
+
+"use strict";
+
+//-----------------------------------------------------------------------------
+// Requirements
+//-----------------------------------------------------------------------------
+
+const assert = require("chai").assert;
+const { Env } = require("../src/env");
+
+//-----------------------------------------------------------------------------
+// Tests
+//-----------------------------------------------------------------------------
+
+describe("Env", () => {
+
+    describe("new Env()", () => {
+
+        it("should set source property to process.env when called without an argument", () => {
+            const env = new Env();
+            assert.strictEqual(env.source, process.env);
+        });
+
+        it("should set source property to argument when called with an argument", () => {
+            const source = {};
+            const env = new Env(source);
+            assert.strictEqual(env.source, source);
+        });
+
+    });
+
+    describe("get()", () => {
+
+        const source = {
+            USERNAME: "humanwhocodes"
+        };
+
+        it("should get an environment variable when it exists", () => {
+            const env = new Env(source);
+            const value = env.get("USERNAME");
+            assert.strictEqual(value, source.USERNAME);
+        });
+
+        it("should return null when the environment variable doesn't exist and there's no default", () => {
+            const env = new Env(source);
+            const value = env.get("PASSWORD");
+            assert.isNull(value);
+        });
+
+        it("should return 123 when the environment variable doesn't exist and 123 is the default", () => {
+            const env = new Env(source);
+            const value = env.get("PASSWORD", 123);
+            assert.strictEqual(value, 123);
+        });
+
+
+    });
+
+    describe("require()", () => {
+
+        const source = {
+            USERNAME: "humanwhocodes"
+        };
+
+        it("should get an environment variable when it exists", () => {
+            const env = new Env(source);
+            const value = env.require("USERNAME");
+            assert.strictEqual(value, source.USERNAME);
+        });
+
+        it("should throw an error when the environment variable doesn't exist", () => {
+            const env = new Env(source);
+
+            assert.throws(() => {
+                env.require("PASSWORD");
+            }, /PASSWORD/);
+        });
+
+    });
+
+});
