bdba1484ba6a089c5a40b382f31d35fc6ed33a2a
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2021-03-01 17:46:59 -0800

    Add Node.js 14

---
+++ rollup.config.d.ts
@@ -0,0 +1,16 @@
+declare var _default: ({
+    input: string;
+    output: {
+        file: string;
+        format: string;
+    }[];
+    plugins?: undefined;
+} | {
+    input: string;
+    plugins: any[];
+    output: {
+        file: string;
+        format: string;
+    };
+})[];
+export default _default;
