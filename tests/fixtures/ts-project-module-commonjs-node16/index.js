0a8bc77d4c652701ac0a3e769f2a3bd6cb31eef3
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2024-04-15 16:49:07 -0700

    fix: Creates CJS type declarations for CJS export.
    fixes #136

---
+++ tests/fixtures/ts-project-module-commonjs-node16/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const env_1 = require("@humanwhocodes/env");
+const env = new env_1.Env();
+const username = env.get("USERNAME");
